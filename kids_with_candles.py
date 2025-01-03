074eba2419b7cf10a21476e6f9e15099ca4329fb
Daisuke Takahashi <daisuke332003@gmail.com>
2025-01-03 09:25:44 -0800

    Add KidsWithCandles.

---
+++ kids_with_candles.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+# 1431. Kids With the Greatest Number of Candies
+# https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/?envType=study-plan-v2&envId=leetcode-75
+class KidsWithCandles:
+
+    @staticmethod
+    def solve(candies: List[int], extraCandies: int) -> List[bool]:
+        n = len(candies)
+        result = [False] * n
+        max_num = max(candies)
+        for i in range(n):
+            result[i] = (candies[i] + extraCandies >= max_num)
+        return result
---
+++ test_kids_with_candles.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from kids_with_candles import KidsWithCandles
+
+
+class TestKidsWithCandles(TestCase):
+
+    def test_solve(self):
+        candies = [2, 3, 5, 1, 3]
+        extraCandies = 3
+        expected = [True, True, True, False, True]
+        self.assertListEqual(expected, KidsWithCandles.solve(candies, extraCandies))
+        candies = [4, 2, 1, 1, 2]
+        extraCandies = 1
+        expected = [True, False, False, False, False]
+        self.assertListEqual(expected, KidsWithCandles.solve(candies, extraCandies))
+        candies = [12, 1, 12]
+        extraCandies = 10
+        expected = [True, False, True]
+        self.assertListEqual(expected, KidsWithCandles.solve(candies, extraCandies))

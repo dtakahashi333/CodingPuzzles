fd36f32ab4e63681f9e81a7b50aa50eff11e11e2
Daisuke Takahashi <daisuke332003@gmail.com>
2025-02-05 11:20:44 -0800

    Update Permutations.

--- permutations.py
+++ permutations.py
@@ -12,23 +12,45 @@ class Permutations:
     @classmethod
     def solve(cls, nums: List[int]) -> List[List[int]]:
         st, output = [], []
-        for cur in nums:
-            cls.helper(nums, cur, st, output)
+        cls.helper(nums, st, output)
         return output
 
     @classmethod
-    def helper(cls, nums: List[int], cur: int, st: List[int], output: List[List[int]]) -> None:
-        st.append(cur)
+    def helper(cls, nums: List[int], st: List[int], output: List[List[int]]) -> None:
+        if len(st) == len(nums):
+            output.append(st.copy())
+            return
 
-        count = 0
         for val in nums:
             if val not in st:
-                cls.helper(nums, val, st, output)
-                count += 1
+                st.append(val)
+                cls.helper(nums, st, output)
+                st.pop()
 
-        if count == 0:
-            # This is the last item.
-            output.append(st.copy())
+    #
+    # The algorithm does not generate lexicographically ordered permutations.
+    #
+    @classmethod
+    def solve2(cls, nums: List[int]) -> List[List[int]]:
+        output = []
+        cls.helper2(nums, 0, output)
+        return output
 
-        # Remove the current item.
-        st.pop()
+    @classmethod
+    def helper2(cls, nums: List[int], k: int, output: List[List[int]]):
+        n = len(nums)
+
+        if k == n - 1:
+            output.append(nums.copy())
+            return
+
+        for i in range(k, n):
+            # Swap
+            tmp = nums[i]
+            nums[i] = nums[k]
+            nums[k] = tmp
+            cls.helper2(nums, k + 1, output)
+            # Roll back
+            tmp = nums[i]
+            nums[i] = nums[k]
+            nums[k] = tmp
--- test_permutations.py
+++ test_permutations.py
@@ -18,3 +18,18 @@ class TestPermutations(TestCase):
         nums = [1]
         expected = [[1]]
         self.assertListEqual(expected, Permutations.solve(nums))
+
+    def test_solve2_1(self):
+        nums = [1, 2, 3]
+        expected = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+        self.assertCountEqual(expected, Permutations.solve2(nums))
+
+    def test_solve2_2(self):
+        nums = [0, 1]
+        expected = [[0, 1], [1, 0]]
+        self.assertCountEqual(expected, Permutations.solve2(nums))
+
+    def test_solve2_3(self):
+        nums = [1]
+        expected = [[1]]
+        self.assertCountEqual(expected, Permutations.solve2(nums))

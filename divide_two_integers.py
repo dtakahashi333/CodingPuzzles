d292ffb04b76fda1f543175b2452c4e8b33d6468
Daisuke Takahashi <daisuke332003@gmail.com>
2025-02-05 11:22:44 -0800

    Update DivideTwoIntegers.

--- divide_two_integers.py
+++ divide_two_integers.py
@@ -1,5 +1,6 @@
 # 29. Divide Two Integers
 # https://leetcode.com/problems/divide-two-integers/
+# https://en.wikipedia.org/wiki/Division_algorithm
 class DivideTwoIntegers:
 
     @classmethod
@@ -29,4 +30,44 @@ class DivideTwoIntegers:
             if remainder >= divisor:
                 remainder = remainder - divisor
                 quotient = quotient | 1 << i
+        return quotient
+
+    @classmethod
+    def solve2(cls, dividend: int, divisor: int) -> int:
+        if divisor == 1:
+            return dividend
+        sign = 1
+        if dividend < 0:
+            sign = ~sign + 1
+            dividend = ~dividend + 1
+        if divisor < 0:
+            sign = ~sign + 1
+            divisor = ~divisor + 1
+        quotient = cls.long_division2(dividend, divisor)
+        if sign < 0:
+            return ~quotient + 1  # To make the value negative
+        else:
+            return min(quotient, 2 ** 31 - 1)  # 2147483647 (32-bit integer-maximum)
+
+    @classmethod
+    def long_division2(cls, dividend: int, divisor: int) -> int:
+        # Find out the most significant place value.
+        place_value = 0
+        m = dividend
+        while m > 0:
+            m = m // 10
+            place_value = place_value + 1
+
+        m = dividend
+        quotient = 0
+        remainder = 0
+        for i in range(place_value - 1, -1, -1):
+            quotient *= 10
+            remainder *= 10
+            remainder += (m // int(pow(10, i)))
+            m = m % int(pow(10, i))
+            if remainder >= divisor:
+                while remainder >= divisor:
+                    remainder -= divisor
+                    quotient += 1
         return quotient
--- test_divide_two_integers.py
+++ test_divide_two_integers.py
@@ -5,44 +5,86 @@ import math
 
 class TestDivideTwoIntegers(TestCase):
 
-    def test_solve1(self):
+    def test_solve_1(self):
         dividend = 10
         divisor = 3
         expected = math.trunc(dividend / divisor)
         self.assertEquals(expected, DivideTwoIntegers.solve(dividend, divisor))
 
-    def test_solve2(self):
+    def test_solve_2(self):
         dividend = 7
         divisor = -3
         expected = math.trunc(dividend / divisor)
         self.assertEquals(expected, DivideTwoIntegers.solve(dividend, divisor))
 
-    def test_solve3(self):
+    def test_solve_3(self):
         dividend = 1
         divisor = 1
         expected = math.trunc(dividend / divisor)
         self.assertEquals(expected, DivideTwoIntegers.solve(dividend, divisor))
 
-    def test_solve4(self):
+    def test_solve_4(self):
         dividend = -2147483648
         divisor = -1
         expected = 2147483647
         self.assertEquals(expected, DivideTwoIntegers.solve(dividend, divisor))
 
-    def test_solve5(self):
+    def test_solve_5(self):
         dividend = 2147483647
         divisor = 2
         expected = math.trunc(dividend / divisor)
         self.assertEquals(expected, DivideTwoIntegers.solve(dividend, divisor))
 
-    def test_solve6(self):
+    def test_solve_6(self):
         dividend = -2147483648
         divisor = 1
         expected = math.trunc(dividend / divisor)
         self.assertEquals(expected, DivideTwoIntegers.solve(dividend, divisor))
 
-    def test_solve7(self):
+    def test_solve_7(self):
         dividend = -2147483648
         divisor = 2
         expected = math.trunc(dividend / divisor)
         self.assertEquals(expected, DivideTwoIntegers.solve(dividend, divisor))
+
+    def test_solve2_1(self):
+        dividend = 10
+        divisor = 3
+        expected = math.trunc(dividend / divisor)
+        self.assertEquals(expected, DivideTwoIntegers.solve2(dividend, divisor))
+
+    def test_solve2_2(self):
+        dividend = 7
+        divisor = -3
+        expected = math.trunc(dividend / divisor)
+        self.assertEquals(expected, DivideTwoIntegers.solve2(dividend, divisor))
+
+    def test_solve2_3(self):
+        dividend = 1
+        divisor = 1
+        expected = math.trunc(dividend / divisor)
+        self.assertEquals(expected, DivideTwoIntegers.solve2(dividend, divisor))
+
+    def test_solve2_4(self):
+        dividend = -2147483648
+        divisor = -1
+        expected = 2147483647
+        self.assertEquals(expected, DivideTwoIntegers.solve2(dividend, divisor))
+
+    def test_solve2_5(self):
+        dividend = 2147483647
+        divisor = 2
+        expected = math.trunc(dividend / divisor)
+        self.assertEquals(expected, DivideTwoIntegers.solve2(dividend, divisor))
+
+    def test_solve2_6(self):
+        dividend = -2147483648
+        divisor = 1
+        expected = math.trunc(dividend / divisor)
+        self.assertEquals(expected, DivideTwoIntegers.solve2(dividend, divisor))
+
+    def test_solve2_7(self):
+        dividend = -2147483648
+        divisor = 2
+        expected = math.trunc(dividend / divisor)
+        self.assertEquals(expected, DivideTwoIntegers.solve2(dividend, divisor))
